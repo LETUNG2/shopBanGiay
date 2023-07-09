0b4903cbb04f5ef09a87491cbc3c5afce350e26b
Le Tung <letung2572002@gmail.com>
2023-07-09 23:37:36 +0700

    release

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="MyShoes/css/hd.css">
+    <link rel="stylesheet" href="MyShoes/css/trangchu.css">
+    <link rel="stylesheet" type="text/css" href="MyShoes/css/ft.css">
+    <title>MyShoes</title>
+</head>
+
+<body>
+
+    <?php
+    include("connect.php");
+    include('MyShoes/header.php');
+    include('MyShoes/trangchu.php');
+    include('MyShoes/footer.php');
+    ?>
+</body>
+
+</html>
